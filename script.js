258d9d606763dc6082b21a1751693ad6cb7fa38f
kaaripie <81368494+kaaripie@users.noreply.github.com>
2021-03-26 22:25:49 +0500

    --

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-var input_val;
+  // ----------- парсинг json ---------------- //
+  let data;
 
-function add_del(id, user_name, user_id) {
-  var _l_id = document.getElementById(user_id).value;
-  var _l_name = document.getElementById(user_name).value;
-  // ----------- рабочий парсинг списка контактов ---------------- //
   var source = $.getJSON( "/result.json", function() {
-      console.log( "getJSON" );
+    console.log( "getJSON" );
+  })
+    .done(function(imported) {
+      //console.log( "success" );
+      data = imported;
     })
-      .done(function() {
-        //console.log( "success" );
-      })
-      .fail(function() {
-        alert( "error" );
-      })
-      .always(function() {
-        //console.log( "complete" );
-      });
-  // ----------
+    .fail(function() {
+      alert( "error" );
+    })
+    .always(function() {
+      //console.log( "complete" );
+    });
+// ----------
+
+function get_id(id, user_name) {
+  var _l_name = document.getElementById(user_name).value;
     
-  source.done(function(data) {
-        //console.log( "processing" );
-        /*
-        //список имен фамилий моих контактов
-        for (var i = 0; i < data.contacts.list.length; i++) {
-          console.log( data.contacts.list[i].last_name + " " + data.contacts.list[i].last_name );
-        }
-        */
-        // число сообщений от контакта по клику на кнопку "отправить"
-        for (var i = 0; i < data.chats.list.length; i++) {
-          if (data.chats.list[i].name == _l_name || data.chats.list[i].id == _l_id)
-            console.log( data.chats.list[i].messages.length );
-        }
-  });
-  return true;
+  //список имен моих контактов
+  for (var i = 0; i < data.frequent_contacts.list.length; i++) {
+    if (_l_name)
+    alert( "ID пользователя: " + data.data.frequent_contacts.list[i].id );
+  }
+}
+
+function count_messages(id, user_id) {
+  var _l_id = document.getElementById(user_id).value;
+
+  // количество сообщений  
+  for (var i = 0; i < data.chats.list.length; i++) {
+    if (data.chats.list[i].name == _l_name || data.chats.list[i].id == _l_id)
+      alert ( "В чате с " + _l_name + " " + data.chats.list[i].messages.length + "сообщений" );
+  }
 }
