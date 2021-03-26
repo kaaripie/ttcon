9c30f89a536b1e9464ed1ed7289641fd07a1f2b1
kaaripie <81368494+kaaripie@users.noreply.github.com>
2021-03-26 23:13:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -32,11 +32,25 @@ function count_messages(id, user_name) {
   var _l_name = document.getElementById(user_name).value;
   var _l_id = get_user_id(_l_name);
 
-  // количество сообщений  
+  // показать количество сообщений
   if (_l_id)
   for ( var i = 0; i < data.chats.list.length; i++ ) {
     if ( data.chats.list[i].id == _l_id ) {
-      alert ( "В этом чате " + data.chats.list[i].messages.length + " сообщений\r\n\r" + "id пользователя: " + _l_id);
+      alert ( "В чате " + data.chats.list[i].messages.length + " сообщений\r\n\r" + "ID чата: " + _l_id);
     }
+  }
+}
+
+function download_chat(id, user_name) {
+  var _l_name = document.getElementById(user_name).value;
+  var _l_id = get_user_id(_l_name);
+
+  // выгрузить все сообщения в консоль
+  if (_l_id)
+  let msgs = chats.list.id[_l_id].messages;
+
+  for ( var i = 0; i < all_messages.length; i++ ) {
+      var _l_item = msgs[i];
+      console.log ( "[" + _l_item.date.replase("T", ", ") + "]" + _l_item.from + ": " +_l_item.text );
   }
 }
