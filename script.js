60d0ddf6afe9ad4012247f19b6e60d7daf57a7f0
kaaripie <81368494+kaaripie@users.noreply.github.com>
2021-03-26 21:44:29 +0500

    добавляю кнопку со вводом имени

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-var source = $.getJSON( "/copy.json", function() {
-    console.log( "getJSON success" );
-  })
-    .done(function(data) {
-      console.log( "done" );
-      console.log( data.name );
+(function() {
+  var adduser_input=document.getElementById('add_id_user');
+  document.getElementById('formIdName').addEventListener('submit',call_add);
+  function call_add() {
+      return add_del(adduser_input.value);
+  }
+})();
+
+function ad_del(id, user_id) {
+  var _l_name = document.getElementById(user_id).value;
+  // ----------- рабочий парсинг списка контактов ---------------- //
+  var source = $.getJSON( "/result.json", function() {
+      console.log( "getJSON" );
     })
-    .fail(function() {
-      alert ( "error" );
-    })
-    .always(function() {
-      console.log( "complete" );
-    });
+      .done(function() {
+        console.log( "success" );
+      })
+      .fail(function() {
+        alert( "error" );
+      })
+      .always(function() {
+        console.log( "complete" );
+      });
+  // ----------
+    
+  source.done(function(data) {
+        console.log( "done" );
+        /*
+        //список имен фамилий моих контактов
+        for (var i = 0; i < data.contacts.list.length; i++) {
+          console.log( data.contacts.list[i].last_name + " " + data.contacts.list[i].last_name );
+        }
+        */
+       
+        for (var i = 0; i < data.chats.list.length; i++) {
+          if (data.chats.list[i].name == _l_name)
+            console.log( data.chats.list[i].messages.length );
+        }
+  });
+
+}
