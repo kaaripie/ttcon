701bd56521fcfdee0ce6abb03278b1a5cde17c7f
kaaripie <81368494+kaaripie@users.noreply.github.com>
2021-03-26 22:42:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,18 +20,22 @@ function get_id(id, user_name) {
   var _l_name = document.getElementById(user_name).value;
     
   //список имен моих контактов
-  for (var i = 0; i < data.frequent_contacts.list.length; i++) {
-    if (_l_name == data.frequent_contacts.list[i].name )
+  for ( var i = 0; i < data.frequent_contacts.list.length; i++ ) {
+    if ( _l_name == data.frequent_contacts.list[i].name ) {
       alert( "ID пользователя: " + data.frequent_contacts.list[i].id );
-    } else alert("нет такого пользователя, скопируй полное имя из чата");
+    } else {
+      alert( "нет такого пользователя, скопируй полное имя из чата" );
+    }
+  }
 }
 
 function count_messages(id, user_id) {
   var _l_id = document.getElementById(user_id).value;
 
   // количество сообщений  
-  for (var i = 0; i < data.chats.list.length; i++) {
-    if (data.chats.list[i].name == _l_name || data.chats.list[i].id == _l_id)
+  for ( var i = 0; i < data.chats.list.length; i++ ) {
+    if ( data.chats.list[i].name == _l_name || data.chats.list[i].id == _l_id ) {
       alert ( "В чате с " + _l_name + " " + data.chats.list[i].messages.length + "сообщений" );
+    }
   }
 }
