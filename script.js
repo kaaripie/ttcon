83f3f6138efe6d9dc8264550cf39fe62699a7050
kaaripie <81368494+kaaripie@users.noreply.github.com>
2021-03-26 22:49:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,26 +16,26 @@
       });
 // ----------
 
-function get_id(id, user_name) {
-  var _l_name = document.getElementById(user_name).value;
-  console.log(_l_name)  
+function get_user_id(in_name) {
   //список имен моих контактов
   for ( var i = 0; i < data.frequent_contacts.list.length; i++ ) {
-    if ( data.frequent_contacts.list[i].name == _l_name ) {
-      alert( "ID пользователя: " + data.frequent_contacts.list[i].id );
-      break;
+    if ( data.frequent_contacts.list[i].name == in_name ) {
+      return data.frequent_contacts.list[i].id;
     } else {
-      alert( "нет такого пользователя, скопируй полное имя из чата" );
+      alert( "нет такого пользователя" );
+      return null;
     }
   }
 }
 
-function count_messages(id, user_id) {
-  var _l_id = document.getElementById(user_id).value;
+function count_messages(id, user_name) {
+  var _l_name = document.getElementById(user_name).value;
+  var _l_id = get_user_id(_l_name);
 
   // количество сообщений  
+  if (_l_id)
   for ( var i = 0; i < data.chats.list.length; i++ ) {
-    if ( data.chats.list[i].name == _l_name || data.chats.list[i].id == _l_id ) {
+    if ( data.chats.list[i].id == _l_id ) {
       alert ( "В чате с " + _l_name + " " + data.chats.list[i].messages.length + "сообщений" );
     }
   }
